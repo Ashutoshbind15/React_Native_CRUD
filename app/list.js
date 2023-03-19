11c1ae0bd9d4f904e8078dd1836cbf948b84adc3
Ashutoshbind15 <91549538+Ashutoshbind15@users.noreply.github.com>
2023-03-20 03:24:07 +0530

    cleanup

--- app/list.js
+++ app/list.js
@@ -6,47 +6,12 @@ import { useTodos } from "../query/hooks/queries";
 import { useTodoMutations } from "../query/hooks/mutations";
 
 const List = () => {
-  const [data, setData] = useState();
-
-  // useEffect(() => {
-  //   const todoRef = collection(FIRESTORE_DB, "todos");
-
-  //   const sub = onSnapshot(todoRef, {
-  //     next: (snapshot) => {
-  //       const t = [];
-
-  //       snapshot.docs.forEach((doc) => {
-  //         t.push({
-  //           id: doc.id,
-  //           ...doc.data(),
-  //         });
-  //       });
-
-  //       setData(t);
-  //     },
-  //   });
-
-  //   //data but not realtime
-  //   // const fetcher = async () => {
-  //   //   const t = [];
-  //   //   const qs = await getDocs(collection(FIRESTORE_DB, "todos"));
-  //   //   qs.forEach((el) => {
-  //   //     t.push({ ...el.data(), id: el.id });
-  //   //   });
-  //   //   setData(t);
-  //   // };
-
-  //   // fetcher();
-
-  //   return () => sub();
-  // }, []);
-
   const { todos, isLoading, isError } = useTodos();
   const { addMutation } = useTodoMutations();
   console.log(todos);
 
   const adder = () => {
-    addMutation.mutate({ title: "Hle", isDone: true });
+    addMutation.mutate({ title: "Hello", isDone: true });
   };
 
   return (
